fc842fcb619b663afbb55f25aa73e83bb7a26a56
biabt <bbtavora@gmail.com>
2025-07-19 21:49:00 -0300

    delete back end

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Platform } from "react-native";
 
 const api = axios.create({
-  baseURL: "http://10.0.2.2:8000",
+  baseURL: "https://mentuca-back-production.up.railway.app",
   withCredentials: false,
 });
 
